a6f7a5f575b7da487217491c53419c7c291019a8
UCLAShawn3G <uclashawn1@gmail.com>
2019-03-24 15:51:34 -0700

    file contents, database setup

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES 
+("Husk", true),
+("In-N-Out", true),
+("Shake Shack", true),
+("Five Guys", true),
+("WhiteCastle", true),
+("The Spotted Pig", true); 
