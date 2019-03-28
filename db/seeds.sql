29e65e8de297de64ce5ee0023114da0472db23ec
UCLAShawn3G <uclashawn1@gmail.com>
2019-03-28 01:49:25 -0700

    seeds new static data

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,8 +3,10 @@
 INSERT INTO burgers (burger_name, devoured) 
 VALUES 
 ("Husk", true),
+("McDonalds", false),
 ("In-N-Out", true),
 ("Shake Shack", true),
 ("Five Guys", true),
 ("WhiteCastle", true),
+("Burger King", false),
 ("The Spotted Pig", true); 
